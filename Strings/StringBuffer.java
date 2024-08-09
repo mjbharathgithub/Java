f631e8affc8af996e157197649d8242b307f716f
Joseph Bharath <112538236+mjbharathgithub@users.noreply.github.com>
2024-08-09 12:39:06 +0530

    Create StringBuffer.java

---
+++ Strings/StringBuffer.java
@@ -0,0 +1,37 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Sample
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		String name="Joseph Bharath";
+		//System.out.println(name);
+		StringBuffer sb= new StringBuffer();
+// 		sb.append('a');
+// 		sb.append(" bharath");
+// 		sb.insert(0,"bharath is ");
+// 		System.out.println(sb);
+// 		sb.replace(13,19,"true");
+// // 		sb.reverse();
+// 		sb.delete(0,6);
+// 		System.out.println(sb);
+// 		System.out.println(sb.charAt(0));
+       // sb.append(sb);
+       
+       sb.append("123456789123456789 123");
+       
+       System.out.println(sb.capacity());
+        // System.out.println(sb.getClass().getSimpleName());
+        // System.out.println(((Object)51).getClass().getSimpleName());
+        // System.out.println(((Object)51.43545).getClass().getSimpleName());
+        // System.out.println(((Object)'A').getClass().getSimpleName());
+        
+		
+	}
+}
